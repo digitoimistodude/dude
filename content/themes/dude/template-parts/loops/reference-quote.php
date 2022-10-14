6bace36276e8ac0bf440f886fc343e915182f0b2
Roni Laukkarinen <roni@dude.fi>
2022-10-14 11:37:56 +0300

    Attempt to add class

--- content/themes/dude/template-parts/loops/reference-quote.php
+++ content/themes/dude/template-parts/loops/reference-quote.php
@@ -3,7 +3,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2022-04-23 16:15:21
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2022-10-10 15:38:28
+ * @Last Modified time: 2022-10-14 11:35:55
  *
  * @package dude
  */
@@ -20,6 +20,25 @@ $data = [
     'company'   => get_the_title( $args['post_id'] ),
   ],
 ];
+
+$picture_cdn_args = [
+  'width'   => '635',
+  'height'  => '635',
+  'classes' => [ 'person' ],
+];
+
+$picture_cdn_srcset = [
+  220 => [
+    'width'     => '64',
+    'height'    => '64',
+  ],
+  768 => [
+    'width'     => '64',
+    'height'    => '64',
+  ],
+];
+
+
 ?>
 
 <li class="item item-quote item-quote-<?php echo esc_html( sanitize_title( $data['person']['company'] ) ); ?> item-quote-big swiper-slide"<?php if ( is_front_page() || is_page( 9 ) ) echo 'style="background-color: ' . esc_attr( $data['color'] ) . ';"'; ?>>
@@ -30,16 +49,7 @@ $data = [
     </blockquote>
 
     <figcaption>
-      <picture>
-        <source media="(min-width: 320px) and (max-width: 767px)" srcset="<?php echo esc_url( wp_get_attachment_image_url( $data['person']['image'], 'thumbnail' ) ); ?>">
-        <source media="(min-width: 768px)" srcset="<?php echo esc_url( wp_get_attachment_image_url( $data['person']['image'], 'thumbnail' ) ); ?>">
-        <img
-          class="person"
-          loading="lazy"
-          src="<?php echo esc_url( wp_get_attachment_image_url( $data['person']['image'], 'thumbnail' ) ); ?>"
-          alt="<?php echo esc_attr( get_post_meta( $data['person']['image'], '_wp_attachment_image_alt', true ) ); ?>"
-        >
-      </picture>
+      <?php get_picture_element_with_cfcdn( $data['person']['image'], $picture_cdn_args, $picture_cdn_srcset ); ?>
 
       <cite>
         <span class="name"><span class="mdash" aria-hidden="true"></span><?php echo esc_html( $data['person']['name'] ) ?><span class="comma">, </span></span><span class="name-title"><?php echo esc_html( strtolower( $data['person']['job_title'] ) ) ?></span>
