431d57b6901dc47e91a70b9401240a27c0677de7
Roni Laukkarinen <roni@dude.fi>
2022-10-14 12:39:04 +0300

    Add image block sizes

--- content/themes/dude/template-parts/blocks/image.php
+++ content/themes/dude/template-parts/blocks/image.php
@@ -6,8 +6,8 @@
  *
  * @Author:		Roni Laukkarinen
  * @Date:   		2022-02-10 12:28:36
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2022-07-29 11:52:37
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2022-10-14 12:38:45
  *
  * @package dude
  */
@@ -24,12 +24,30 @@ if ( empty( $image_id ) ) {
   maybe_show_error_block( 'Kuvaa ei ole asetettu' );
   return;
 }
+
+$picture_cdn_args = [
+  'width'     => '1400',
+  'height'    => '865',
+];
+
+$picture_cdn_srcset = [
+  220 => [
+    'width'     => '435',
+    'height'    => '269',
+  ],
+  600 => [
+    'width'     => '555',
+    'height'    => '343',
+  ],
+  1000 => [
+    'width'     => '1400',
+    'height'    => '865',
+  ],
+];
 ?>
 
 <section class="block block-image has-unified-padding-if-stacked">
   <div class="container has-transition-fade">
-
-    <?php native_lazyload_tag( $image_id ) ?>
-
+    <?php get_picture_element_with_cfcdn( $image_id, $picture_cdn_args, $picture_cdn_srcset ); ?>
   </div>
 </section>
