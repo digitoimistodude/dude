d70aecba3313766ba4079d194845b4cec792e498
Roni Laukkarinen <roni@dude.fi>
2022-10-14 10:45:03 +0300

    Add official image in place of single test

--- content/themes/dude/template-parts/loops/reference.php
+++ content/themes/dude/template-parts/loops/reference.php
@@ -3,7 +3,7 @@
  * @Author: Timi Wahalahti
  * @Date:   2022-04-23 15:45:23
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2022-10-14 10:38:24
+ * @Last Modified time: 2022-10-14 10:44:32
  * @package dude
  */
 
@@ -23,51 +23,30 @@ $data = [
 
   <a class="global-link" href="<?php echo esc_url( $data['permalink'] ) ?>" aria-hidden="true" tabindex="-1"></a>
 
-  <div class="image image-background has-duotone" aria-hidden="true">
-
-    <?php
-    if ( 'development' === getenv( 'WP_ENV' ) && 12530 === $args['post_id'] ) : ?>
-
-      <!-- <picture>
-        <source media="(min-width: 220px) and (max-width: 599px)" srcset="https://cdn.dude.fi/cdn-cgi/image/width=450,height=450,quality=75,fit=cover,format=auto/https://www.dude.fi/media/sofi_oksanen_19b8714-scaled-1.jpg">
-        <source media="(min-width: 600px) and (max-width: 759px)" srcset="https://cdn.dude.fi/cdn-cgi/image/width=678,height=678,quality=75,fit=cover,format=auto/https://www.dude.fi/media/sofi_oksanen_19b8714-scaled-1.jpg">
-        <source media="(min-width: 760px) and (max-width: 899px)" srcset="https://cdn.dude.fi/cdn-cgi/image/width=320,height=320,quality=75,fit=cover,format=auto/https://www.dude.fi/media/sofi_oksanen_19b8714-scaled-1.jpg">
-        <source media="(min-width: 900px) and (max-width: 1439px)" srcset="https://cdn.dude.fi/cdn-cgi/image/width=500,height=500,quality=75,fit=cover,format=auto/https://www.dude.fi/media/sofi_oksanen_19b8714-scaled-1.jpg">
-        <source media="(min-width: 1440px)" srcset="https://cdn.dude.fi/cdn-cgi/image/width=635,height=635,quality=75,fit=cover,format=auto/https://www.dude.fi/media/sofi_oksanen_19b8714-scaled-1.jpg">
-        <img
-          loading="lazy"
-          src="https://cdn.dude.fi/cdn-cgi/image/width=635,height=635,quality=75,fit=cover,format=auto/https://www.dude.fi/media/sofi_oksanen_19b8714-scaled-1.jpg"
-          alt=""
-        >
-      </picture> -->
-      <?php get_picture_element_with_cfcdn( get_post_thumbnail_id( 12530 ), [
-        'width'   => '635',
-        'height'  => '635',
-        'quality' => '75',
-        'fit'      => 'cover',
-      ], [
-        220 => [
-          'width'     => '450',
-          'height'    => '450',
-        ],
-        600 => [
-          'width'     => '678',
-          'height'    => '678',
-        ],
-        1349 => [
-          'width'     => '500',
-          'height'    => '500',
-        ],
-        760 => [
-          'width'     => '320',
-          'height'    => '450',
-        ],
-      ] ); ?>
-
-    <?php else : ?>
-      <?php native_lazyload_tag( $data['thumbnail_id'] ) ?>
-    <?php endif; ?>
-
+  <div class="image image-background has-duotone">
+    <?php get_picture_element_with_cfcdn( $data['thumbnail_id'], [
+      'width'   => '635',
+      'height'  => '635',
+      'quality' => '75',
+      'fit'      => 'cover',
+    ], [
+      220 => [
+        'width'     => '450',
+        'height'    => '450',
+      ],
+      600 => [
+        'width'     => '678',
+        'height'    => '678',
+      ],
+      1349 => [
+        'width'     => '500',
+        'height'    => '500',
+      ],
+      760 => [
+        'width'     => '320',
+        'height'    => '450',
+      ],
+    ] ); ?>
   </div>
 
   <h3 class="has-text-gradient">
