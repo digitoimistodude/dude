d8bc107825b4b3314d946694e7203c095ac67d52
Roni Laukkarinen <roni@dude.fi>
2022-10-14 12:20:25 +0300

    Add image sizes to gallery

--- content/themes/dude/template-parts/blocks/title-images.php
+++ content/themes/dude/template-parts/blocks/title-images.php
@@ -7,7 +7,7 @@
  * @Author:		Roni Laukkarinen
  * @Date:   		2022-02-10 12:28:36
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2022-08-27 17:31:05
+ * @Last Modified time: 2022-10-14 12:19:59
  *
  * @package dude
  */
@@ -54,11 +54,142 @@ if ( empty( $title ) ) {
     </div>
 
     <div class="images">
-      <?php foreach ( $images as $image ) : ?>
+      <?php $count = 1; foreach ( $images as $image ) : ?>
         <div class="image">
-          <?php native_lazyload_tag( $image ); ?>
+          <?php if ( 1 === $count ) {
+              $picture_cdn_args = [
+                'width'   => '700',
+                'height'  => '467',
+              ];
+
+              $picture_cdn_srcset = [
+                220 => [
+                  'width'     => '218',
+                  'height'    => '145',
+                ],
+                480 => [
+                  'width'     => '218',
+                  'height'    => '145',
+                ],
+                600 => [
+                  'width'     => '278',
+                  'height'    => '185',
+                ],
+                760 => [
+                  'width'     => '429',
+                  'height'    => '286',
+                ],
+                1160 => [
+                  'width'     => '656',
+                  'height'    => '437',
+                ],
+                1591 => [
+                  'width'     => '700',
+                  'height'    => '467',
+                ],
+              ];
+
+            } elseif ( 2 === $count ) {
+              $picture_cdn_args = [
+                'width'     => '525',
+                'height'    => '350',
+              ];
+
+              $picture_cdn_srcset = [
+                220 => [
+                  'width'     => '163',
+                  'height'    => '109',
+                ],
+                620 => [
+                  'width'     => '252',
+                  'height'    => '168',
+                ],
+                1000 => [
+                  'width'     => '370',
+                  'height'    => '207',
+                ],
+                1460 => [
+                  'width'     => '525',
+                  'height'    => '350',
+                ],
+              ];
+            } elseif ( 3 === $count ) {
+              $picture_cdn_args = [
+                'width'     => '500',
+                'height'    => '333',
+              ];
+
+              $picture_cdn_srcset = [
+                220 => [
+                  'width'     => '218',
+                  'height'    => '145',
+                ],
+                620 => [
+                  'width'     => '263',
+                  'height'    => '175',
+                ],
+                1000 => [
+                  'width'     => '429',
+                  'height'    => '286',
+                ],
+                1200 => [
+                  'width'     => '500',
+                  'height'    => '333',
+                ],
+              ];
+            } elseif ( 4 === $count ) {
+              $picture_cdn_args = [
+                'width'     => '900',
+                'height'    => '450',
+              ];
+
+              $picture_cdn_srcset = [
+                220 => [
+                  'width'     => '288',
+                  'height'    => '144',
+                ],
+                620 => [
+                  'width'     => '372',
+                  'height'    => '186',
+                ],
+                1000 => [
+                  'width'     => '720',
+                  'height'    => '360',
+                ],
+                1201 => [
+                  'width'     => '900',
+                  'height'    => '450',
+                ],
+              ];
+            } else {
+              $picture_cdn_args = [
+                'width'     => '525',
+                'height'    => '350',
+              ];
+
+              $picture_cdn_srcset = [
+                220 => [
+                  'width'     => '163',
+                  'height'    => '109',
+                ],
+                620 => [
+                  'width'     => '252',
+                  'height'    => '168',
+                ],
+                1000 => [
+                  'width'     => '370',
+                  'height'    => '207',
+                ],
+                1460 => [
+                  'width'     => '525',
+                  'height'    => '350',
+                ],
+              ];
+            }
+            ?>
+            <?php get_picture_element_with_cfcdn( $image, $picture_cdn_args, $picture_cdn_srcset ); ?>
         </div>
-      <?php endforeach; ?>
+      <?php $count++; endforeach; ?>
     </div>
 
   </div>
