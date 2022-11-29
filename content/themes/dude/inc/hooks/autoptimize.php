20a5722d42402099674a681f31339b5e5d7b418d
Roni Laukkarinen <roni@dude.fi>
2022-11-29 19:32:58 +0200

    Add note about critical CSS and FCP

--- content/themes/dude/inc/hooks/autoptimize.php
+++ content/themes/dude/inc/hooks/autoptimize.php
@@ -13,6 +13,7 @@
 namespace Air_Light;
 
 // Load minifications after Yoast meta tags
+// If this is set to '</body>', it also moves critical CSS to footer which is not good for FCP
 function autoptimize_tweaks( $replacetag ) {
   return array( '</head>', 'before' );
 }
