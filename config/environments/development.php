fcf11b02dd87f51b87a1c2e42c9cccb39a1bccbb
Roni Laukkarinen <roni@dude.fi>
2024-02-19 13:34:40 +0200

    Hide warnings and deprecated notices for development

--- config/environments/development.php
+++ config/environments/development.php
@@ -6,14 +6,16 @@
 use Roots\WPConfig\Config;
 
 Config::define( 'SAVEQUERIES', true );
-Config::define( 'WP_DEBUG', false );
-Config::define( 'WP_DEBUG_DISPLAY', false );
+Config::define( 'WP_DEBUG', true );
+Config::define( 'WP_DEBUG_DISPLAY', true );
 Config::define( 'WP_DISABLE_FATAL_ERROR_HANDLER', false );
-Config::define( 'SCRIPT_DEBUG', false );
+Config::define( 'SCRIPT_DEBUG', true );
 Config::define( 'FS_METHOD', 'direct' );
 Config::define( 'PLL_CACHE_HOME_URL', false );
 
-ini_set( 'display_errors', '0' );
+//ini_set( 'display_errors', '0' );
+// Hide warnings and deprecated notices for development
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 // Enable plugin and theme updates and installation from the admin
 Config::define( 'DISALLOW_FILE_MODS', false );
