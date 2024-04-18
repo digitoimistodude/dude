23117ddd16de4de5a865e05d657f9d9273878837
Roni Laukkarinen <roni@dude.fi>
2024-04-18 15:06:09 +0300

    Add @2x support for CDN images

--- content/themes/dude/inc/hooks/scripts-styles.php
+++ content/themes/dude/inc/hooks/scripts-styles.php
@@ -118,7 +118,7 @@ function get_asset_file( $filename ) {
 
   $filetype = pathinfo( $filename )['extension'];
 
-  return "${filetype}/${env}/${filename}";
+  return "{$filetype}/{$env}/{$filename}";
 } // end get_asset_file
 
 /**
--- content/themes/dude/inc/template-tags/picture-with-cf-cdn.php
+++ content/themes/dude/inc/template-tags/picture-with-cf-cdn.php
@@ -1,21 +1,14 @@
 <?php
 /**
- * File: picture-with-cfcd.php
- *
- * Description: picture-with-cfcd
- *
- * @Author:        Elias Kautto
- * @Date:           2022-10-13 15:43:28
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2022-10-14 15:57:36
+ * Cloudflare CDN support (image resizing)
+ * Version: 2.0.0 (2024-04-18)
  *
  * @package dude
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
 
 namespace Air_Light;
 
-function get_picture_element_with_cfcdn( $image_id, $img_params, $sources ) {
+function get_picture_element_with_cfcdn( $image_id, $img_params, $sources, $loading = 'lazy' ) {
   if ( ! isset( $sources ) || empty( $sources ) ) {
     return;
   }
@@ -24,11 +17,45 @@ function get_picture_element_with_cfcdn( $image_id, $img_params, $sources ) {
     return;
   }
 
-  // remove filter to avoid getting cdn url already in this stage
+  // Remove filter to avoid getting cdn url already in this stage
   remove_filter( 'wp_get_attachment_image_src', __NAMESPACE__ . '\change_attachment_image_src_to_cfcdn' );
 
   $image_data = wp_get_attachment_image_src( $image_id, 'full' );
   $image_url = $image_data[0];
+
+  $crop_dimensions = get_post_meta( $image_id, '_wpsmartcrop_enabled', true ) ? get_post_meta( $image_id, '_wpsmartcrop_image_focus', true ) : [];
+  if ( empty( $crop_dimensions ) ) {
+    $styles['focal-position'] = 'auto';
+    $image_object_position = false;
+  } else {
+
+    // Set dimensions for gravity setting and reverse array (top comes first in array)
+    $parsed_dimensions = array_map(
+      function ( $dimension ) {
+
+        $dimension = round( $dimension, 0 );
+
+        if ( 10 > $dimension ) {
+          return '0.0' . $dimension;
+        } else {
+          return '0.' . $dimension;
+        }
+      },
+      array_reverse( $crop_dimensions )
+    );
+
+    // Set dimensions for inline css object-position and reverse array (top comes first in array)
+    $parsed_object_dimensions = array_map(
+      function ( $dimension ) {
+        return $dimension . '%';
+      },
+      array_reverse( $crop_dimensions )
+    );
+
+    $styles['focal-position'] = implode( 'x', $parsed_dimensions );
+    $image_object_position = $cssstyles['object-position'] = implode( ' ', $parsed_object_dimensions );
+
+  }
 
   add_filter( 'wp_get_attachment_image_src', __NAMESPACE__ . '\change_attachment_image_src_to_cfcdn' );
 
@@ -37,6 +64,7 @@ function get_picture_element_with_cfcdn( $image_id, $img_params, $sources ) {
     'height'  => $image_data[2],
     'quality' => THEME_SETTINGS['cfcdn_defaults']['quality'],
     'fit'     => THEME_SETTINGS['cfcdn_defaults']['fit'],
+    'gravity' => $styles['focal-position'],
     'classes' => [],
   ] );
 
@@ -50,7 +78,7 @@ function get_picture_element_with_cfcdn( $image_id, $img_params, $sources ) {
 
   ksort( $sources );
 
-  echo '<picture>';
+  echo '<div class="image image-background image-background-layer"><picture>';
   foreach ( $sources as $key => $source ) {
     if ( ! isset( $source['width'] ) || ! isset( $source['height'] ) ) {
       continue;
@@ -68,34 +96,50 @@ function get_picture_element_with_cfcdn( $image_id, $img_params, $sources ) {
       'width'   => $source['width'],
       'height'  => $source['height'],
       'quality' => $img_params['quality'],
+      'gravity' => $img_params['gravity'],
       'fit'     => $img_params['fit'],
     ] );
 
-    echo '<source media="' . $media .'" srcset="' .$image_cdn_url . '">'; //phpcs:ignore
+    $image_cdn_url_retina = build_image_cf_cdn_url( $image_url, [
+      'width'   => $source['width'] * 2,
+      'height'  => $source['height'] * 2,
+      'quality' => $img_params['quality'],
+      'gravity' => $img_params['gravity'],
+      'fit'     => $img_params['fit'],
+    ] );
+
+    echo '<source media="' . $media .'" srcset="' .$image_cdn_url . ' 1x, ' .$image_cdn_url_retina . ' 2x">'; // phpcs:ignore
   }
 
   $image_cdn_url = build_image_cf_cdn_url( $image_url, $img_params );
 
-  echo '<img ' . $img_params['classes'] . 'loading="lazy" src="' . $image_cdn_url . '" width="' . $img_params['width'] . '" height="' . $img_params['height'] . '" alt="' . $alt . '">'; //phpcs:ignore
-  echo '</picture>';
-} // get_picture_element_with_cfcd
-
-function build_image_cf_cdn_url( $image_url, $args = [] ) {
-  unset( $args['classes'] );
-
-  if ( ! isset( $args['format'] ) ) {
-    $args['format'] = 'auto';
+  if ( ! empty( $image_object_position ) ) {
+    echo '<img ' . $img_params['classes'] . 'loading="' . $loading . '" src="' . $image_cdn_url . '" width="' . $img_params['width'] . '" height="' . $img_params['height'] . '" alt="' . $alt . '" style="object-position: '. $image_object_position .'">'; //phpcs:ignore
+  } else {
+    echo '<img ' . $img_params['classes'] . 'loading="' . $loading . '" src="' . $image_cdn_url . '" width="' . $img_params['width'] . '" height="' . $img_params['height'] . '" alt="' . $alt . '">'; //phpcs:ignore
   }
 
-  $args_url = [];
-  foreach ( $args as $key => $value ) {
-    $args_url[] = "{$key}={$value}";
-  }
+  echo '</picture></div>';
+}
 
-  // CF CDN does not support loading from local (duh), get same image from production
-  if ( 'production' !== wp_get_environment_type() ) {
-    $image_url = str_replace( 'dude.test', 'dude.fi', $image_url );
-  }
+if ( ! function_exists( '\Air_Light\build_image_cf_cdn_url' ) ) {
+  function build_image_cf_cdn_url( $image_url, $args = [] ) {
+    unset( $args['classes'] );
 
-  return 'https://cdn.dude.fi/cdn-cgi/image/' . implode( ',', $args_url ) . "/{$image_url}";
-} // end build_image_cf_cdn_url
+    if ( ! isset( $args['format'] ) ) {
+      $args['format'] = 'auto';
+    }
+
+    $args_url = [];
+    foreach ( $args as $key => $value ) {
+      $args_url[] = "{$key}={$value}";
+    }
+
+    // CF CDN does not support loading from local (duh), get same image from production
+    if ( 'production' !== wp_get_environment_type() ) {
+      $image_url = str_replace( 'dude.test', 'dude.fi', $image_url );
+    }
+
+    return 'https://cdn.dude.fi/cdn-cgi/image/' . implode( ',', $args_url ) . "/{$image_url}";
+  } // End build_image_cf_cdn_url
+}
