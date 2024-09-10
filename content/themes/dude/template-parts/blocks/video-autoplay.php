7513cae34d79186d138742592e94174e5e7e2254
Roni Laukkarinen <roni@dude.fi>
2024-09-10 19:49:17 +0300

    Attempt to fix autoplay on production

--- content/themes/dude/template-parts/blocks/video-autoplay.php
+++ content/themes/dude/template-parts/blocks/video-autoplay.php
@@ -40,16 +40,30 @@ $video_data = wp_remote_get( 'https://api.vimeo.com/videos/' . $video_id, [
 if ( is_wp_error( $video_data ) ) {
   $video_width = 1920;
   $video_height = 1080;
-  $player_embed_url = 'https://player.vimeo.com/video/' . $video_id;
 } else {
   $video_data = json_decode( wp_remote_retrieve_body( $video_data ) );
   $video_width = $video_data->width;
   $video_height = $video_data->height;
-  $player_embed_url = $video_data->player_embed_url;
 }
+
+// The actual URL
+$player_embed_url = 'https://player.vimeo.com/video/' . $video_id;
+
+// Make it loop and autoplay
+$player_embed_url = add_query_arg( [
+  'title' => 0,
+  'portrait' => 0,
+  'muted' => 1,
+  'autoplay' => 1,
+  'byline' => 0,
+  'controls' => 0,
+  'background' => 1,
+  'dnt' => 1,
+  'loop' => 1,
+  'autopause' => 0,
+], $player_embed_url );
 ?>
 
 <section class="block block-video-autoplay">
-  <iframe class="reference-iframe" src="<?php echo esc_url( $player_embed_url ); ?>?autoplay=1&loop=1&muted=1&background=1&dnt=1" width="<?php echo esc_attr( $video_width ); ?>" height="<?php echo esc_attr( $video_height ); ?>" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+  <iframe class="reference-iframe" src="<?php echo esc_url( $player_embed_url ); ?>" width="<?php echo esc_attr( $video_width ); ?>" height="<?php echo esc_attr( $video_height ); ?>" frameborder="0" allow="fullscreen" allow="autoplay"></iframe>
 </section>
-
