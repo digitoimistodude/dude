f3189249cecd501a0f403c4f771c2bfd2cfa9d56
Roni Laukkarinen <roni@dude.fi>
2024-09-12 18:14:06 +0300

    Add crisp chat

--- content/themes/dude/inc/hooks/analytics.php
+++ content/themes/dude/inc/hooks/analytics.php
@@ -23,6 +23,7 @@ function head_analytics_scripts() { ?>
   </script>
 
   <?php if ( 'production' === wp_get_environment_type() ) : ?>
+    <script data-swup-ignore-script>window.$crisp=[];window.CRISP_WEBSITE_ID="-K90vfAnyk27kD-pZAep";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
     <script data-swup-ignore-script>(function(l,e,a,d,i,n,f,o){if(!l[i]){l.GlobalLeadinfoNamespace=l.GlobalLeadinfoNamespace||[];l.GlobalLeadinfoNamespace.push(i);l[i]=function(){(l[i].q=l[i].q||[]).push(arguments)};l[i].t=l[i].t||n;l[i].q=l[i].q||[];o=e.createElement(a);f=e.getElementsByTagName(a)[0];o.async=1;o.src=d;f.parentNode.insertBefore(o,f);}}(window,document,'script','https://cdn.leadinfo.net/ping.js','leadinfo','LI-66461A90231DA'));</script>
     <script data-swup-ignore-script defer data-domain="dude.fi" src="https://analytics.dude.fi/js/plausible.js"></script>
   <?php endif; ?>
