4d98464e6ec1b194f548850cdc3c5b97371720e2
Roni Laukkarinen <roni@dude.fi>
2025-04-02 03:17:23 +0300

    Responsiveness, add more content

--- content/themes/dude/template-upkeep-landing.php
+++ content/themes/dude/template-upkeep-landing.php
@@ -67,22 +67,72 @@ get_header(); ?>
   <section class="block block-upkeep-price" style="background: transparent;">
     <div class="container">
       <div class="items">
-        <div class="item item-glow">
-          <a href="<?php echo esc_url( get_permalink( 4487 ) ); ?>" class="global-link" aria-hidden="true" tabindex="-1"></a>
+        <div class="item-wrapper">
+          <div class="item item-price item-glow">
+            <a href="<?php echo esc_url( get_permalink( 4487 ) ); ?>" class="global-link" aria-hidden="true" tabindex="-1"></a>
 
-          <p class="badge" aria-hidden="true" tabindex="-1">
-            Ota yhteyttä ja tilaa
-          </p>
+            <p class="badge" aria-hidden="true" tabindex="-1">
+              Ota yhteyttä ja tilaa
+            </p>
 
-          <h2><a href="<?php echo esc_url( get_permalink( 4487 ) ); ?>" aria-label="199 &euro;, ota yhteyttä ja tilaa">199 &euro;</a></h2>
-          <p>per WordPress-verkkopalvelu</p>
-          <ul>
-            <li>Multisite-alasivustot +79 € / kpl</li>
-            <li>Toistaiseksi voimassaoleva ylläpitosopimus.</li>
-            <li>Laskutus kolmen kuukauden välein etukäteen.</li>
-            <li>Jos käyttöönottovaiheessa ilmenee merkittäviä esteitä ylläpitoon ottoa varten ja palvelua ei voida toimittaa, emme veloita mitään ja sopimus puretaan.</li>
-            <li>Hinnat voimassa 12 kuukautta sopimuksen allekirjoittamisesta.</li>
-          </ul>
+            <h2><a href="<?php echo esc_url( get_permalink( 4487 ) ); ?>" aria-label="199 &euro;, ota yhteyttä ja tilaa">199 &euro; <span class="text-small">/kk</span></a></h2>
+            <p>per WordPress-verkkopalvelu</p>
+            <ul>
+              <li>Multisite-alasivustot +79 € / kpl</li>
+              <li>Toistaiseksi voimassaoleva ylläpitosopimus.</li>
+              <li>Laskutus kolmen kuukauden välein etukäteen.</li>
+              <li>Jos käyttöönottovaiheessa ilmenee merkittäviä esteitä ylläpitoon ottoa varten ja palvelua ei voida toimittaa, emme veloita mitään ja sopimus puretaan.</li>
+              <li>Hinnat voimassa 12 kuukautta sopimuksen allekirjoittamisesta.</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="item-wrapper">
+          <div class="item item-details">
+
+            <div class="upsell">
+              <svg width="32" height="32" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m24 43.697-19.176-20a11.344 11.344 0 0 1-2.126-13.102v0a11.346 11.346 0 0 1 18.17-2.948L24 10.777l3.132-3.13a11.346 11.346 0 0 1 16.046 16.044L24 43.697Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+              <h3>Ripeä tukipalvelu</h3>
+              <p>Meillä ei vastaa mikään robotti, vaan asiantuntijamme poikkeuksellisen nopeasti - jopa tuntien sisällä viestistä. Kysyminen ei maksa mitään.</p>
+            </div>
+
+            <div class="upsell">
+              <svg width="32" height="32" fill="none" viewBox="0 0 48 49" xmlns="http://www.w3.org/2000/svg"><path d="M3.412 24c0 8.15 4.736 15.193 11.604 18.53L5.195 15.621a20.51 20.51 0 0 0-1.783 8.38ZM37.9 22.962c0-2.545-.913-4.307-1.697-5.679-1.044-1.696-2.022-3.132-2.022-4.828 0-1.893 1.436-3.654 3.457-3.654.092 0 .178.01.267.016a20.513 20.513 0 0 0-13.903-5.405c-7.193 0-13.522 3.69-17.203 9.28.483.015.938.025 1.325.025 2.153 0 5.487-.261 5.487-.261 1.11-.066 1.24 1.565.132 1.696 0 0-1.115.131-2.357.196l7.498 22.302 4.506-13.514-3.208-8.789a37.578 37.578 0 0 1-2.159-.196c-1.11-.065-.98-1.761.13-1.696 0 0 3.4.261 5.424.261 2.153 0 5.487-.261 5.487-.261 1.11-.065 1.24 1.565.132 1.696 0 0-1.118.131-2.357.196l7.44 22.133 2.054-6.863c.89-2.847 1.568-4.893 1.568-6.655Z" fill="currentColor"></path><path d="m24.361 25.8-6.177 17.951a20.59 20.59 0 0 0 12.653-.328 1.809 1.809 0 0 1-.146-.284l-6.33-17.338ZM42.068 14.121c.088.656.138 1.36.138 2.117 0 2.09-.39 4.438-1.565 7.375l-6.29 18.183C40.474 38.226 44.59 31.596 44.59 24c0-3.58-.914-6.946-2.523-9.879Z" fill="currentColor"></path><path d="M24 0C10.768 0 0 10.766 0 24c0 13.235 10.767 24.001 24 24.001 13.234 0 24.003-10.766 24.003-24.001C48.003 10.766 37.234 0 24 0Zm0 46.901C11.375 46.901 1.1 36.628 1.1 24 1.1 11.373 11.374 1.1 24 1.1c12.627 0 22.9 10.273 22.9 22.9 0 12.628-10.273 22.901-22.9 22.901Z" fill="currentColor"></path></svg>
+              <h3>WordPress-päivitykset</h3>
+              <p>WordPress lisäosineen ei meillä jää happanemaan. Päivitämme tekniikkaa taustalla säännöllisesti. Päivitykset tarkistaa aina ihminen.</p>
+            </div>
+
+            <div class="upsell">
+              <svg width="32" height="32" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M37.715 19.479H10.286a3.429 3.429 0 0 0-3.429 3.428v20.571a3.429 3.429 0 0 0 3.429 3.43h27.429a3.429 3.429 0 0 0 3.428-3.43v-20.57a3.429 3.429 0 0 0-3.428-3.43Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M32.469 5.867A11.795 11.795 0 0 0 24 2.336a12 12 0 0 0-12 12v5.143M24 34.907a1.714 1.714 0 1 0 0-3.428 1.714 1.714 0 0 0 0 3.428Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+              <h3>Tietoturva</h3>
+              <p>WordPress on maailman suosituin, siksi myös hyökätyin. Meiltä löytyy tekniikkaa, joka pitää sivustosi turvassa. Olemme suojanneet mm. WordPress-kirjautumiset hyökkäysyrityksiltä ja koventaneet WordPressin ympäristöineen.</p>
+            </div>
+
+            <div class="upsell">
+              <svg width="32" height="32" viewBox="0 0 53 54" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.945 30.694a4.417 4.417 0 1 0 8.834 0 4.417 4.417 0 0 0-8.834 0ZM36.195 12.898a19.875 19.875 0 0 1 11.042 17.808M18.528 12.898A19.875 19.875 0 0 0 7.486 30.706M37.837 47.588a19.875 19.875 0 0 1-20.952 0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="m25.859 26.541-5.523-5.251L26.72 3.696a2.509 2.509 0 0 1 4.859 1.049l-1.725 22.304M24.512 34.074l-1.782 7.407L4.297 44.75a2.509 2.509 0 0 1-1.522-4.734l20.191-9.66M31.71 31.472l7.307-2.158 12.047 14.33a2.509 2.509 0 0 1-3.341 3.684l-18.455-12.65" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+              <h3>Toimivuuden valvonta</h3>
+              <p>Verkkopalvelunne on 24/7 valvonnassa automaattisen monitorointityökalun toimesta, joka hälyttää ylläpitotiimiämme jo ennen kuin mahdollinen vikatilanne vaikuttaa verkkopalvelun toimintaan./p>
+            </div>
+
+            <div class="upsell">
+              <svg fill="none" height="32" width="32" viewBox="0 0 38 45" xmlns="http://www.w3.org/2000/svg"><g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m17.5 13.2266 6-6.00004-6-6"></path><path d="m36.9379 23.7266c.04.494.062 1 .062 1.5.0017 1.5179-.1891 3.03-.568 4.5"></path><path d="m32.7881 13.6562c.9773 1.1616 1.8033 2.4425 2.458 3.8121"></path><path d="m32.744 36.8496c-.32.3787-.6586.7467-1.016 1.104-1.0723 1.0744-2.2766 2.0085-3.584 2.78"></path><path d="m20.5 43.1646c-.494.04-1 .062-1.5.062-4.7739 0-9.35227-1.8965-12.72792-5.2721-3.37566-3.3757-5.27208-7.954-5.27208-12.7279s1.89642-9.3523 5.27208-12.728c3.37565-3.37561 7.95402-5.27204 12.72792-5.27204h4.5"></path></g></svg>
+              <h3>Palvelinpäivitykset</h3>
+              <p>Jos palvelin on meidän hoiteissa, pidämme tekniikan ajan tasalla. Muiden palvelimia hoidamme myös kuin omiamme, rajoitteineen.</p>
+            </div>
+
+            <div class="upsell">
+              <svg width="32" height="32" fill="none" viewBox="0 0 49 49" xmlns="http://www.w3.org/2000/svg"><path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M33.078 32.69h10a4 4 0 0 0 4-4v-17a4 4 0 0 0-4-4h-27a4 4 0 0 0-4 4v6M47.078 17.69h-25M30.078 17.69v4"></path><path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M24.078 7.69h12l-.722-4.329a2 2 0 0 0-1.972-1.672h-6.612A2 2 0 0 0 24.8 3.361l-.722 4.328ZM6.578 44.99a7.155 7.155 0 0 1 1.818.26l4.232 1.171a7.159 7.159 0 0 0 1.922.262h5.706a6.928 6.928 0 0 0 6.972-6.124l.35-5.776a5.122 5.122 0 0 0-3.97-5.2l-1.698-.362a1.708 1.708 0 0 1-1.326-1.648v-3.84a2.626 2.626 0 0 0-5.25 0v1.76a8.628 8.628 0 0 1-8.756 8.496v11Z"></path><path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M2.18 31.69h4.4v15h-4.4"></path></svg>
+              <h3>Varmuuskopiointi</h3>
+              <p>Poistitko kuvan, joka pitäisi palauttaa? Onko sisältö hukassa? Ei hätää, kaikki otetaan talteen tunnin välein.</p>
+            </div>
+
+            <div class="upsell">
+              <svg width="32" height="32" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 51"><path d="M41.695 33.693c.411 0 .818.092 1.198.27.38.178.725.439 1.016.768.29.33.52.72.678 1.15a3.954 3.954 0 0 1 0 2.712c-.157.43-.388.82-.678 1.15-.29.329-.636.59-1.016.768-.38.178-.787.27-1.198.27h-2.087c.83 0 1.627.373 2.214 1.038.587.664.917 1.566.917 2.506 0 1.958-1.403 2.362-3.13 2.362H26.042c-5.95 0-7.305-1.181-14.61-2.362V24.243C16.548 24.243 25 13.61 25 4.16c0-3.736 4.569-5.127 6.261 1.698 1.044 4.208-2.087 12.477-2.087 12.477H45.87c.83 0 1.627.374 2.214 1.038.587.665.917 1.566.917 2.506 0 1.959-1.403 4.725-3.13 4.725h-2.088c.83 0 1.627.374 2.214 1.038.587.665.917 1.566.917 2.506 0 .94-.33 1.841-.917 2.506-.587.665-1.384 1.038-2.214 1.038h-2.087Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M11.435 20.697H1v28.351h10.435v-28.35Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6.739 44.324c-.289 0-.522-.264-.522-.59 0-.327.233-.591.522-.591" stroke="currentColor" stroke-width="2"></path><path d="M6.738 44.324c.288 0 .522-.264.522-.59 0-.327-.234-.591-.522-.591" stroke="currentColor" stroke-width="2"></path></svg>
+              <h3>Domainit, DNS ja muut palvelut samasta paikasta</h3>
+              <p>Tarvitseeko päivittää tietueita tai hankkia uusi domain ohjauksineen? Meiltä onnistuu.</p>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
