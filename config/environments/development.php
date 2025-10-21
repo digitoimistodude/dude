f9638b75ef39a0e1eac05832161ceacc9a7cbcd3
Roni Laukkarinen <roni@dude.fi>
2025-10-21 18:29:13 +0300

    Add new dev settings, Ref: DEV-292

--- config/environments/development.php
+++ config/environments/development.php
@@ -5,22 +5,24 @@
 
 use Roots\WPConfig\Config;
 
-Config::define( 'SAVEQUERIES', false );
-Config::define( 'WP_DEBUG', false );
-Config::define( 'WP_DEBUG_DISPLAY', false );
-Config::define( 'WP_DISABLE_FATAL_ERROR_HANDLER', false );
-Config::define( 'SCRIPT_DEBUG', false );
+$root_dir = dirname( dirname( __DIR__ ) );
+
+Config::define( 'SAVEQUERIES', true );
+Config::define( 'WP_DEBUG', true );
+Config::define( 'WP_DEBUG_DISPLAY', true );
+Config::define( 'WP_DEBUG_LOG', $root_dir . '/logs/wp-debug.log' );
+Config::define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true );
+Config::define( 'SCRIPT_DEBUG', true );
 Config::define( 'FS_METHOD', 'direct' );
 Config::define( 'PLL_CACHE_HOME_URL', false );
+Config::define( 'WP_DEVELOPMENT_MODE', 'all' );
 
-//ini_set( 'display_errors', '0' );
-// Hide warnings and deprecated notices for development
-error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+ini_set( 'display_errors', '1' ); // phpcs:ignore
+ini_set( 'log_errors', '1' ); // phpcs:ignore
+ini_set( 'error_log', $root_dir . '/logs/wp-debug.log' ); // phpcs:ignore
+ini_set( 'date.timezone', 'Europe/Helsinki' ); // phpcs:ignore
+ini_set( 'log_errors_max_len', '0' ); // phpcs:ignore
+date_default_timezone_set( 'Europe/Helsinki' ); // phpcs:ignore WordPress.DateTime.RestrictedFunctions.timezone_change_date_default_timezone_set
 
 // Enable plugin and theme updates and installation from the admin
 Config::define( 'DISALLOW_FILE_MODS', false );
-Config::define( 'WP_REDIS_DISABLED', false );
-
-// Developing in the train :)
-// TODO: Disable this later
-Config::define( 'WP_HTTP_BLOCK_EXTERNAL', true );
