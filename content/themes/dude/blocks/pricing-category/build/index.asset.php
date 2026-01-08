ffdd9ba79bf23bf25adc4398c8bd0b68e103e8dc
Roni Laukkarinen <roni@dude.fi>
2026-01-08 14:45:43 +0200

    Fix pricing-category grid layout for editor and frontend, Ref: DEV-567
    - Use CSS Grid instead of flex for two-column layout
    - Grid works for both frontend (with wrappers) and editor (flat blocks)
    - Position heading/paragraph in left column, items in right column

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b21729dc6ba29cc9649d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'af73ca213af862274467');
