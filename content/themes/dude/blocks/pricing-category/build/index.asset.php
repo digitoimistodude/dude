159e4ed73147c1529438790afc70045b0ad60736
Roni Laukkarinen <roni@dude.fi>
2026-01-08 15:04:16 +0200

    Add is-layout-grid class to prevent classic.css margin issues, Ref: DEV-567
    WordPress classic.css adds auto margins to .wp-block elements except
    inside .is-layout-grid containers. This prevents the grid layout from
    breaking in the editor.

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'af73ca213af862274467');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '87592ee5126451c49d5a');
