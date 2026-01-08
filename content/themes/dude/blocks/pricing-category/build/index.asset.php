d1a17ef7769c01a3b6b60ba4fb0873a6ff5c1988
Roni Laukkarinen <roni@dude.fi>
2026-01-08 20:37:53 +0200

    Build: update compiled assets, Ref: DEV-444

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cf4f4605ea8014952a2b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1f8a0fe58ae0b0e95bed');
--- content/themes/dude/blocks/pricing-faq/build/index.asset.php
+++ content/themes/dude/blocks/pricing-faq/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1e7549df3881195ef539');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e0be4d2c931bb46a8490');
