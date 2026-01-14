193a872c45e47c19a01ab6b00aea3c63a330baf7
Roni Laukkarinen <roni@dude.fi>
2026-01-14 17:50:31 +0200

    Fix pricing-category save function to include section wrapper, Ref: DEV-567

--- content/themes/dude/blocks/pricing-category/src/index.js
+++ content/themes/dude/blocks/pricing-category/src/index.js
@@ -1,9 +1,23 @@
 import { registerBlockType } from '@wordpress/blocks';
-import { InnerBlocks } from '@wordpress/block-editor';
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 import Edit from './edit';
 import './style.scss';
 
 registerBlockType( 'dude/pricing-category', {
   edit: Edit,
-  save: () => <InnerBlocks.Content />,
+  save: () => {
+    const blockProps = useBlockProps.save( {
+      className: 'block block-pricing-category has-unified-padding-if-stacked',
+    } );
+
+    return (
+      <section { ...blockProps }>
+        <div className="container">
+          <div className="pricing-category">
+            <InnerBlocks.Content />
+          </div>
+        </div>
+      </section>
+    );
+  },
 } );
