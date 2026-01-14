9d4500b484c860b58a82f28fc058c69e52393e1a
Roni Laukkarinen <roni@dude.fi>
2026-01-14 18:02:35 +0200

    Implement accessible accordion pattern for pricing blocks, Ref: DEV-567

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4013fa5a2a1ee643c5af');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6f9101b2a59ac18debe3');
