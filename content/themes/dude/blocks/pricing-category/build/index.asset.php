13b07f098244189d2300b3f214e321898faf4a02
Roni Laukkarinen <roni@dude.fi>
2026-01-14 17:50:31 +0200

    Fix pricing-category save function to include section wrapper, Ref: DEV-567

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c8398559015661c49b19');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4013fa5a2a1ee643c5af');
