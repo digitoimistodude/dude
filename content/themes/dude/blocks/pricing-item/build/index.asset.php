8da8511113cc5ee04ce386ea14634903e7e5c449
Roni Laukkarinen <roni@dude.fi>
2026-01-19 10:55:29 +0200

    Finalize pricing faq block, Ref: DEV-567

--- content/themes/dude/blocks/pricing-item/build/index.asset.php
+++ content/themes/dude/blocks/pricing-item/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '69ac7c4aca16fbd51866');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '883f3ccc384b55502bde');
