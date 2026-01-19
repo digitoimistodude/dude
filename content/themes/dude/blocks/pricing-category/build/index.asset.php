8843821546745c898f770276f962906f496bc21e
Roni Laukkarinen <roni@dude.fi>
2026-01-19 12:01:47 +0200

    Fix merge conflict after rebase: DEV-567

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '87592ee5126451c49d5a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cf4f4605ea8014952a2b');
--- content/themes/dude/js/src/modules/100vh.js
+++
@@ -1,9 +0,0 @@
-const init100vhMobileFix = () => {
-  // Set vh variable
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-
-export default init100vhMobileFix;
-
-window.addEventListener('resize', init100vhMobileFix);
