6b57236b2f9c6399a81fd9a78052729924c767f1
Roni Laukkarinen <roni@dude.fi>
2026-01-19 10:37:23 +0200

    Block finalizations, Ref: DEV-567

--- content/themes/dude/blocks/pricing-item/build/index.asset.php
+++ content/themes/dude/blocks/pricing-item/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9fd3adfceb898f68b0ad');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '69ac7c4aca16fbd51866');
