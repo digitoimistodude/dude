9501f48176921ee6c08a1fa19af392c324c653f8
Roni Laukkarinen <roni@dude.fi>
2026-01-20 13:28:19 +0200

    Fixes, rebuild blocks, Ref: DEV-567

--- content/themes/dude/blocks/pricing-item/build/index.asset.php
+++ content/themes/dude/blocks/pricing-item/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '883f3ccc384b55502bde');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '36deda9b45ee57671cb3');
