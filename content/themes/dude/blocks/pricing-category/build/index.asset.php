c3cd36ac3636fa2e372f703be8136f31dd9a9ded
Roni Laukkarinen <roni@dude.fi>
2026-01-21 17:30:16 +0200

    Add gradient box styles to pricing-category block, Ref: DEV-567

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c2c1feecda405bfcc1c3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1ae821944ce1bffd945b');
