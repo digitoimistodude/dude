5688a0b3f3e209bb945450ddc0ad44a6a6ac4624
Roni Laukkarinen <roni@dude.fi>
2026-01-27 17:55:41 +0200

    Fix pricing-faq editor text and link colors to match frontend, Ref: DEV-701

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f19dad5fa3f863344aca');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f761c9b1be346d9d6403');
