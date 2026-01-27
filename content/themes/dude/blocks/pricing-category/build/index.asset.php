c58efca997c982548eb76c6bcdfa045073cea639
Roni Laukkarinen <roni@dude.fi>
2026-01-27 17:21:49 +0200

    Fix editor accordion link colors and text styles to match frontend, Ref: DEV-701

--- content/themes/dude/blocks/pricing-category/build/index.asset.php
+++ content/themes/dude/blocks/pricing-category/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6c2bc9649455611f57c5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f19dad5fa3f863344aca');
